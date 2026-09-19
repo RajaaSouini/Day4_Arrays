d3319adf1962f795cb6127b8ca0942fb8a8b9caa
RajaaSouini <rajaasouini@gmail.com>
2026-09-19 14:14:07 +0100

    ajout

--- exercice11.js
+++ exercice11.js
@@ -1,5 +1,14 @@
 //inserer dans un tableau sans splice()
 
 let table = [1, 2 , 4 , 5];
+let indic = 1 ;
+let table2 = [];
 
+for(i = 0 ; i < table.length ; i++){
+    table2[table2.length] = table[i];
+    if (i === indic){
+        table2[table2.length] = 3 ;
+    }
 
+}
+console.log(table2);
