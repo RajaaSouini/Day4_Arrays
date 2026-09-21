a45d1baffe5d2b2a18fad072fd12439d0681a717
RajaaSouini <rajaasouini@gmail.com>
2026-09-21 12:08:16 +0000

    math.round

--- exercice21.js
+++ exercice21.js
@@ -1,1 +1,10 @@
-// à refaire
+let table = [4 , 7 , 9 , 6 , 9 , 5];
+let somme = 0;
+
+for(i in table){
+    somme += table[i];
+}
+let result = somme / table.length;
+
+console.log(Math.round(result * 100)/100);
+
---
+++ exercice36.js
@@ -0,0 +1,9 @@
+let table = [1, 1, 2, 2, 2, 3, 1, 1, 1, 1];
+let table2 = [];
+let count = 0;
+
+for(i = 0 ; i< table.length ; i++){
+    if (table[i] === x){
+        count += 1;
+    }
+}
