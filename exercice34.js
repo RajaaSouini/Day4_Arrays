e1f2b8ae9a08bdaef8957928679ca0d512ee3e7b
RajaaSouini <rajaasouini@gmail.com>
2026-09-21 10:24:45 +0000

    ex34

---
+++ exercice34.js
@@ -0,0 +1,24 @@
+let table = [1, 2, 3];
+let table2 = [3, 4, 5];
+
+let table3 = [];
+
+for(i in table2){
+    table.push(table2[i]);
+}
+
+for(i = 0 ; i<table.length ; i++){
+
+    let exist = false;
+
+    for(j = 0 ; j< table3.length ; j++){
+        if (table[i] === table3[j]){
+            exist = true;
+            break;
+        }
+    }
+    if(!exist){
+       table3.push(table[i]);
+    }
+}
+console.log(table3);
