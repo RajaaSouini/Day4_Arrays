6db8b50a2c8de354e10751b27845ee9ec75f7d2b
RajaaSouini <rajaasouini@gmail.com>
2026-09-22 11:33:06 +0000

    apresvirgule

--- exercice21.js
+++ exercice21.js
@@ -5,6 +5,12 @@ for(i in table){
     somme += table[i];
 }
 let result = somme / table.length;
+let result2 = result * 100 ;
+let intResult = parseInt(result2);
 
-console.log(Math.round(result * 100)/100);
+//console.log(Math.round(result * 100)/100);
+
+//faire le role de Math.round(result * 100)/100  manuellement 
+
+console.log(intResult/100);
 
