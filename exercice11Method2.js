a51f2dbebee7fc5c817d87ded1d86ebf2a38e5a6
RajaaSouini <rajaasouini@gmail.com>
2026-09-22 15:29:13 +0000

    ex11method2

---
+++ exercice11Method2.js
@@ -0,0 +1,18 @@
+//insérons dans un tableau qui doit etre trier 
+
+let table = [1, 2 , 4 , 5 ];
+
+let elem = 3 ;
+table.unshift(elem);
+let i = 0;
+while(i < table.length-1 && table[i] > table[i+1]){
+    let temp = table[i];
+    table[i] = table[i+1];
+    table[i+1] = temp;
+
+    i++ ; 
+}
+
+
+
+console.log(table);
