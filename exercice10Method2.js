108b4cd69f7ee7a53f5c071e1f21ab972003889a
RajaaSouini <rajaasouini@gmail.com>
2026-09-22 12:18:38 +0000

    edit ex10

--- exercice10Method2.js
+++ exercice10Method2.js
@@ -1,13 +1,16 @@
 let table = [10 , 20 , 30 , 40 , 50];
-let table2 = []; 
-let elem = 30 ;
-let index ;
 
-for (i = 0 ; i< table.length ; i++){
-    if (table[i] === elem){
-        index = i;
-        break;
-    }
+let elem = 10 ; 
+let indice = 0 ;
+for (let i = 0 ; i < table.length ; i++){
+  if (table[i] === elem){
+    indice = i ;
+  }
 }
 
-console.log(index); 
+for(let j = indice ;j < table.length ; j++ ){
+   table[j] = table[j+1];
+}
+table.pop();
+
+console.log(table);
